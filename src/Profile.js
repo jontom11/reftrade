1d54e0d630b12ad2eaebc6cf665f2cbdb34dd742
Jonathan Tom <Jonathan.Y.Tom1@gmail.com>
2017-08-15 00:15:01 -0700

    adding text imput from material-ui

--- src/Profile.js
+++ src/Profile.js
@@ -1,9 +1,78 @@
 import React, {Component} from 'react';
+import TextField from 'material-ui/TextField';
 
 class Profile extends Component {
+  constructor () {
+    super();
+    this.state = {
+      yourName: null,
+      yourEmail: null,
+      yourPhone: null,
+      theirName: null,
+      theirEmail: null,
+      theirPhone: null,
+    }
+    this.nameChange = this.nameChange.bind(this);
+    this.emailChange = this.emailChange.bind(this);
+  }
+
+  nameChange (n) {
+    console.log(n.target.value)
+    this.setState ({name: n.target.value})
+  }
+
+    emailChange (e) {
+    console.log(e.target.value)
+    this.setState ({email: e.target.value})
+  }
+
+
+
   render() {
     return (
-      <div>You Got the Route Baby!</div>
+      <div> 
+        <h3>You Got the Route Baby!</h3>
+        <div>
+          <TextField
+            floatingLabelText="Your First and Last Name"
+            /*errorText="This field is required"*/
+            onChange={this.nameChange}
+          />
+        </div>
+        <div>
+          <TextField
+            floatingLabelText="Your Email Address"
+            onChange={this.emailChange}
+          />
+        </div>
+        <div>
+          <TextField
+            floatingLabelText="Your Phone Number"
+            onChange={this.emailChange}
+          />
+        </div>
+        <div>
+          <TextField
+            floatingLabelText="Referral's First and Last Name"
+            onChange={this.emailChange}
+          />
+        </div>
+        <div>
+          <TextField
+            floatingLabelText="Referral's Email Address"
+            onChange={this.emailChange}
+          />
+        </div>
+        <div>
+          <TextField
+            floatingLabelText="Referral's Phone Number"
+            onChange={this.emailChange}
+          />
+        </div>
+
+
+
+      </div>
     )
   }
 
